06ce75f1ba63a480cc5ae3b2d3a49bda4b6562e6
oscarheimdahl <o.heimdahl@gmail.com>
2021-02-16 22:59:43 +0100

    Cool wallpaper wip

--- src/app/wallpaper/wallpaper.component.ts
+++ src/app/wallpaper/wallpaper.component.ts
@@ -6,9 +6,29 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./wallpaper.component.scss'],
 })
 export class WallpaperComponent implements OnInit {
-  @Input() value: any;
-
+  transition: boolean = false;
+  hideTransition: boolean = false;
+  colors = ['#9AC4F8', '#99EDCC', '#CB958E', '#E36588', '#9A275A'];
+  wallpaperColor: string = this.colors[0];
+  nextWallpaperColor: string = this.colors[1];
+  colorIndex = 0;
+  transitioning = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  showTransition(): void {
+    if (this.transition) return;
+    this.transition = true;
+
+    setTimeout(() => {
+      this.wallpaperColor = this.nextWallpaperColor;
+      this.nextWallpaperColor = this.colors[this.colorIndex];
+      this.hideTransition = true;
+      this.transition = false;
+      this.colorIndex++;
+      if (this.colorIndex > this.colors.length - 1) this.colorIndex = 0;
+      this.hideTransition = false;
+    }, 1500);
+  }
 }
